b850b871846eeac6daf0f91e9c4e65d76b74b6ad
Andrew <stratus.africa@outlook.com>
2026-08-18 23:58:28 +0300

    Update _app.expenses.tsx

--- src/routes/_app.expenses.tsx
+++ src/routes/_app.expenses.tsx
@@ -1,14 +1,18 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { FeatureGate } from "@/components/FeatureGate";
 import { PermissionGuard } from "@/components/routing/helpers";
-import Expenses from "@/pages/Expenses";
+import ChartOfAccounts from "@/pages/ChartOfAccounts";
 
-export const Route = createFileRoute("/_app/expenses")({
+export const Route = createFileRoute("/_app/chart-of-accounts")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
   return (
-    <PermissionGuard permission="expenses.view"><FeatureGate featureKey="expenses"><Expenses /></FeatureGate></PermissionGuard>
+    <PermissionGuard moduleKey="accounting" route="/chart-of-accounts" permission="chart_of_accounts.view">
+      <FeatureGate moduleKey="chart_of_accounts">
+        <ChartOfAccounts />
+      </FeatureGate>
+    </PermissionGuard>
   );
 }
