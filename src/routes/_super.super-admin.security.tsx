ea2fc9fc644910bbb60a2ac775d4796832ffea71
Andrew <stratus.africa@outlook.com>
2026-08-23 01:19:15 +0300

    Add files via upload

---
+++ src/routes/_super.super-admin.security.tsx
@@ -0,0 +1,3 @@
+import { createFileRoute } from '@tanstack/react-router';
+import SuperAdminSecurity from '@/pages/super-admin/SuperAdminSecurity';
+export const Route = createFileRoute('/_super/super-admin/security')({ component: SuperAdminSecurity });
