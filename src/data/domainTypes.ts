4228dbd74a206b6f87cf3cdb6b0036fd189a637f
Andrew <stratus.africa@outlook.com>
2026-08-23 07:17:25 +0300

    Create domainTypes.ts

---
+++ src/data/domainTypes.ts
@@ -0,0 +1,2 @@
+export type PosSettings = { pos_require_manager_to_remove_item?: boolean | null; digitax_enabled?: boolean | null; };
+export type LocationPosSettings = { pos_require_manager_to_remove_item?: boolean | null; };
