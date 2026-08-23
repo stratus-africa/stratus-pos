00fbea16925770d85fe12235b2af15f6a69cb7a7
Andrew <stratus.africa@outlook.com>
2026-08-23 05:21:07 +0300

    Delete src/routes/_super.super-admin.support[1].tsx

--- src/routes/_super.super-admin.support[1].tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import SuperAdminSupport from "@/pages/super-admin/SuperAdminSupport";
-
-export const Route = createFileRoute("/_super/super-admin/support")({
-  component: SuperAdminSupport,
-});
